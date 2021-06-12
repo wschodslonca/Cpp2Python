9f277f6b2fcd2080af80e0dd517d6761419f3c30
PawelHanzlik <hanzlik_pawel@o2.pl>
2021-06-12 16:13:19 +0200

    poprawki

---
+++ workspace/zmienneGlobalne.py
@@ -0,0 +1,18 @@
+# from zmienneGlobalne.cpp to zmienneGlobalne.py
+
+a = 5
+
+
+def add(a, b):
+    return a + b
+
+
+b = 10
+
+
+def main():
+    print("Wynik: ", add(a,b), sep='', end='')
+
+
+if __name__ == '__main__':
+    main()
